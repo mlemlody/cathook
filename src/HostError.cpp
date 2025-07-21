79abb12d91fdc9d1dec917679c3e5cd168720b93
mlemlody <garciaisheree@gmail.com>
2025-07-22 00:15:28 +0200

    Update HostError.cpp

--- src/HostError.cpp
+++ src/HostError.cpp
@@ -8,7 +8,7 @@ static DetourHook host_error_detour;
 
 extern "C" void __attribute__((cdecl)) Host_Error_hook(const char *pInMessage, ...)
 {
-    logging::Info("Suppressed Host_Error: %s", pInMessage ? pInMessage : "(null)");
+    (void)pInMessage;
 }
 
 static InitRoutine host_error_init([]() {
